e01b2038238b37ac0e0899f12cae8c61cde075f1
Jorge Ortegón <jorge_barcelino@hotmail.com>
2022-11-08 02:37:54 -0600

    Animación 15

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -3,14 +3,6 @@ window.sr = ScrollReveal();
 
 let windowSize = window.innerWidth;
 
-// if (windowSize > 768) {
-//   sr.reveal(".navbar__list", {
-//     duration: 3000,
-//     origin: "right",
-//     distance: "300 px"
-//   });
-// }
-
 sr.reveal(".vector__container", {
   duration: 2500,
   origin: "left",
@@ -134,7 +126,82 @@ else {
 
   sr.reveal(".cv__container", {
     duration: 750,
-    origin: "bottom",
+    origin: "top",
     distance: "100 px"
   });
+
+  // test
+
+  sr.reveal(".habilities", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".about__container", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".academy__container--item:nth-of-type(odd)", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".academy__container--item:nth-of-type(even)", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".habilities__info", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".my-habilities__container--items:nth-of-type(odd)", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".portfolio--title", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".portfolio__container--card:nth-of-type(odd)", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".portfolio__container--card:nth-of-type(even)", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".contact__info", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".contact__container--items", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
+  sr.reveal(".footer__title", {
+    duration: 750,
+    origin: "top",
+    distance: "100 px"
+  });
+
 }
