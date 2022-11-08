afefa5ae295e00fc9979bcb07dd14e36f9815cb5
Jorge Ortegón <jorge_barcelino@hotmail.com>
2022-11-08 00:49:13 -0600

    animación de la página

---
+++ assets/js/animation.js
@@ -0,0 +1,111 @@
+window.sr = ScrollReveal();
+
+
+let windoSize = window.innerWidth;
+
+if (windoSize > 768) {
+  sr.reveal(".navbar__list", {
+    duration: 3000,
+    origin: "right",
+    distance: "300 px"
+  });
+}
+
+sr.reveal(".vector__container", {
+  duration: 2500,
+  origin: "left",
+  distance: "300 px"
+});
+
+sr.reveal(".presentation__container--info", {
+  duration: 2000,
+  origin: "top",
+  distance: "300 px"
+});
+
+sr.reveal(".social__container--items", {
+  duration: 2000,
+  origin: "right",
+  distance: "300 px"
+});
+
+sr.reveal(".cv__container", {
+  duration: 2000,
+  origin: "right",
+  distance: "300 px"
+});
+
+sr.reveal(".habilities", {
+  duration: 2000,
+  origin: "top",
+  distance: "150 px"
+});
+
+sr.reveal(".about__container", {
+  duration: 2000,
+  origin: "top",
+  distance: "150 px"
+});
+
+sr.reveal(".academy__container--item:nth-of-type(odd)", {
+  duration: 2000,
+  origin: "left",
+  distance: "150 px"
+});
+
+sr.reveal(".academy__container--item:nth-of-type(even)", {
+  duration: 2000,
+  origin: "right",
+  distance: "150 px"
+});
+
+sr.reveal(".habilities__info", {
+  duration: 2000,
+  origin: "top",
+  distance: "150 px"
+});
+
+sr.reveal(".my-habilities__container--items:nth-of-type(odd)", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "150 px"
+});
+
+
+sr.reveal(".portfolio--title", {
+  duration: 2000,
+  origin: "left",
+  distance: "150 px"
+});
+
+
+sr.reveal(".portfolio__container--card:nth-of-type(odd)", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "150 px"
+});
+
+sr.reveal(".portfolio__container--card:nth-of-type(even)", {
+  duration: 2000,
+  origin: "top",
+  distance: "150 px"
+});
+
+sr.reveal(".contact__info", {
+  duration: 2000,
+  origin: "top",
+  distance: "150 px"
+});
+
+sr.reveal(".contact__container--items", {
+  duration: 2000,
+  origin: "top",
+  distance: "150 px"
+});
+
+sr.reveal(".footer__title", {
+  duration: 3000,
+  origin: "right",
+  distance: "300 px"
+});
+
