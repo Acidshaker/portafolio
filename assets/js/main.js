32c6ce3cf2304b199360aa764a82fcb422f7db72
Jorge Ortegón <jorge_barcelino@hotmail.com>
2022-11-10 08:04:33 -0600

    hide menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,16 @@ const btnMenu = document.querySelector('.btn__open')
 const btnClose = document.querySelector('.btn__close')
 const Menu = document.querySelector('.navbar__menu')
 
-
 btnMenu.onclick = function () {
   Menu.classList.add('show--menu')
   btnMenu.classList.add('hidden')
+
+  if (Menu.classList.contains("show--menu")) {
+    Menu.onclick = function () {
+      Menu.classList.remove("show--menu")
+      btnMenu.classList.remove('hidden')
+    }
+  }
 }
 
 btnClose.onclick = function () {
